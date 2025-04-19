b3f1e73fbe2194aa91c8f5c4d2ef8b97a813603f
abdulkader33447 <abdulkader33447@gmail.com>
2025-04-19 17:23:23 +0600

    added children router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-
-import './App.css'
+import { Outlet } from "react-router";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button className='btn btn-primary'>btn</button>
+      
+      <h1 class="text-3xl font-bold underline">Navbar</h1>
+      <Outlet/>
+      <h1 class="text-3xl font-bold underline">Footer</h1>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
